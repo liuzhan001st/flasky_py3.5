f72274fd85cf45d7fc30413a56ad63c626e7fbf8
liuzhan001st <603812167@qq.com>
2016-07-27 22:44:37 +0800

    Chapter 5: Database Use in View Functions (5b)

--- hello.py
+++ hello.py
@@ -26,7 +26,7 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role')
+    users = db.relationship('User', backref='role', lazy='dynamic')
     
     def __repr__(self):
         return '<Role %r>' % self.name
@@ -46,15 +46,20 @@ class NameForm(Form):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username = form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), 
+        known = session.get('known',False))
     
 @app.route('/user/<name>')
 def user(name):
@@ -69,4 +74,5 @@ def internal_server_error(e):
     return render_template('500.html'), 500
     
 if __name__ == '__main__':
+    db.create_all()
     manager.run()
