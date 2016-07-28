65b1a45082682e9456ce1f19f5c13a2a44e4c243
liuzhan001st <603812167@qq.com>
2016-07-28 11:13:37 +0800

    Chapter 5: Database Migration with Flask-Migrate (5d)

--- hello.py
+++ hello.py
@@ -8,6 +8,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 import os
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -77,6 +78,10 @@ def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role)
     
 manager.add_command("shell", Shell(make_context=make_shell_context))
+
+
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     db.create_all()
