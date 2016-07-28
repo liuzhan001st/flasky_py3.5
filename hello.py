5b6f94e34cfadf4fd8e28decef5731c2474a71db
liuzhan001st <603812167@qq.com>
2016-07-28 15:32:47 +0800

    Chapter 6: Sending Asynchronous Email (6b)

--- hello.py
+++ hello.py
@@ -10,6 +10,7 @@ import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_mail import Mail, Message
+from threading import Thread
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -56,12 +57,18 @@ class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
 
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+    
 def send_email(to, subject, template, **kwargs):
         msg = Message(app.config['FLASY_MAIL_SUBJECT_PREFIX'] + subject, 
                       sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
         msg.body = render_template(template + '.txt', **kwargs)
         msg.html = render_template(template +'.html', **kwargs)
-        mail.send(msg)
+        thr = Thread(target=send_async_email, args=[app, msg])
+        thr.start()
+        return thr
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -105,5 +112,4 @@ migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
-    #db.create_all()
     manager.run()
