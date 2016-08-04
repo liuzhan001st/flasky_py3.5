77f9390ddb5e36d5056c325328a1e35e72d67a99
liuzhan001st <603812167@qq.com>
2016-08-04 22:56:25 +0800

    Chapter 8: Account Confirmation (8e)

--- app/auth/views.py
+++ app/auth/views.py
@@ -6,6 +6,23 @@ from ..models import User
 from ..email import send_email
 from .forms import LoginForm, RegistrationForm
 
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        flash('You can now login.')
+        db.session.commit()     # commite here to get an id to be used by token generating
+        token = user.generate_confirmation_token()
+        send_email(user.email, '[账户激活] Please Activate Your Account on Flasky', 
+                  'auth/email/confirm', user=user, token=token) # Put chinese characters in subject to avoid anti-spam mechanism of netease mail service.
+        flash('A confirmation email has been sent to you by email.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
+
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -17,21 +34,42 @@ def login():
         flash('Invalid username or password')
     return render_template('auth/login.html', form=form)
     
-@auth.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(email=form.email.data,
-                    username=form.username.data,
-                    password=form.password.data)
-        db.session.add(user)
-        flash('You can now login.')
-        return redirect(url_for('auth.login'))
-    return render_template('auth/register.html', form=form)
-    
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
     flash('You have been logged out.')
+    return redirect(url_for('main.index'))
+    
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        flash('You have confirmed your account. Thanks!')
+    else:
+        flash('The confirmation link is invalid or has expired.')
+    return redirect(url_for('main.index'))
+    
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated \
+            and not current_user.confirmed \
+            and request.endpoint[:5] != 'auth.':
+        return redirect(url_for('auth.unconfirmed'))
+        
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed:
+        return redirect('main.index')
+    return render_template('auth/unconfirmed.html')
+    
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+    token = current_user.generate_confirmation_token()
+    send_email(current_user.email, '[账户激活] Please Activate Your Account on Flasky',
+               'auth/email/confirm', user=current_user, token=token)
+    flash('A new confirmation email has been sent to you by email.')
     return redirect(url_for('main.index'))
