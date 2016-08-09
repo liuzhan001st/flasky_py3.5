9b3cd62e18fc77db85c06f4f180f9ce5196dacbc
liuzhan001st <603812167@qq.com>
2016-08-09 10:06:38 +0800

    Chapter 8: Account Management (8h)

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,7 +4,8 @@ from . import auth
 from .. import db
 from ..models import User
 from ..email import send_email
-from .forms import LoginForm, RegistrationForm
+from .forms import LoginForm, RegistrationForm, ChangePasswordForm, \
+    PasswordResetRequestForm, PasswordResetForm, ChangeEmailForm
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
@@ -15,9 +16,9 @@ def register():
                     password=form.password.data)
         db.session.add(user)
         flash('You can now login.')
-        db.session.commit()     # commite here to get an id to be used by token generating
+        db.session.commit()     # commit here to get an id to be used by token generating
         token = user.generate_confirmation_token()
-        send_email(user.email, '[账户激活] Please Activate Your Account on Flasky', 
+        send_email(user.email, '[账户激活] Please Activate Your Account on Flasky',
                   'auth/email/confirm', user=user, token=token) # Put chinese characters in subject to avoid anti-spam mechanism of netease mail service.
         flash('A confirmation email has been sent to you by email.')
         return redirect(url_for('auth.login'))
@@ -33,14 +34,14 @@ def login():
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('Invalid username or password')
     return render_template('auth/login.html', form=form)
-    
+
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
     flash('You have been logged out.')
     return redirect(url_for('main.index'))
-    
+
 @auth.route('/confirm/<token>')
 @login_required
 def confirm(token):
@@ -51,20 +52,21 @@ def confirm(token):
     else:
         flash('The confirmation link is invalid or has expired.')
     return redirect(url_for('main.index'))
-    
+
 @auth.before_app_request
 def before_request():
     if current_user.is_authenticated \
             and not current_user.confirmed \
-            and request.endpoint[:5] != 'auth.':
+            and request.endpoint[:5] != 'auth.' \
+            and request.endpoint != 'static':       # ???
         return redirect(url_for('auth.unconfirmed'))
-        
+
 @auth.route('/unconfirmed')
 def unconfirmed():
     if current_user.is_anonymous or current_user.confirmed:
         return redirect('main.index')
     return render_template('auth/unconfirmed.html')
-    
+
 @auth.route('/confirm')
 @login_required
 def resend_confirmation():
@@ -72,4 +74,79 @@ def resend_confirmation():
     send_email(current_user.email, '[账户激活] Please Activate Your Account on Flasky',
                'auth/email/confirm', user=current_user, token=token)
     flash('A new confirmation email has been sent to you by email.')
+    return redirect(url_for('main.index'))
+
+@auth.route('/change-password', methods=['GET', 'POST'])
+@login_required
+def change_password():
+    form = ChangePasswordForm()
+    if form.validate_on_submit():
+        if current_user.verify_password(form.old_password.data):
+            current_user.password = form.password.data
+            db.session.add(current_user)
+            flash('Your password has been updated.')
+            return redirect(url_for('main.index'))
+        else:
+            flash('Invalid password.')
+    return render_template("auth/change_password.html", form=form)
+
+@auth.route('/reset', methods=['GET', 'POST'])
+def password_reset_request():
+    if not current_user.is_anonymous:
+        return redirect(url_for('main.index'))
+    form = PasswordResetRequestForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            token = user.generate_reset_token()
+            send_email(user.email, '[重置密码]Reset Your Password',
+                       'auth/email/reset_password',
+                       user=user, token=token,
+                       next=request.args.get('next'))
+        flash('An email with instructions to reset your password has been '
+              'sent to you.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/reset_password.html', form=form)
+
+@auth.route('/reset/<token>', methods=['GET', 'POST'])
+def password_reset(token):
+    if not current_user.is_anonymous:
+        return redirect(url_for('main.index'))
+    form = PasswordResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is None:
+            return redirect(url_for('main.index'))
+        if user.reset_password(token, form.password.data):
+            flash('Your password has been updated.')
+            return redirect(url_for('auth.login'))
+        else:
+            return redirect(url_for('main.index'))
+    return render_template('auth/reset_password.html', form=form)
+
+@auth.route('/change-email', methods=['GET', 'POST'])
+@login_required
+def change_email_request():
+    form = ChangeEmailForm()
+    if form.validate_on_submit():
+        if current_user.verify_password(form.password.data):
+            new_email = form.email.data
+            token = current_user.generate_email_change_token(new_email)
+            send_email(new_email, '[验证新邮箱]Confirm your email address',
+                       'auth/email/change_email',
+                       user=current_user, token=token)
+            flash('An email with instructions to confirm your new email '
+                  'address has been sent to you.')
+            return redirect(url_for('main.index'))
+        else:
+            flash('Invalid email or password.')
+    return render_template("auth/change_email.html", form=form)
+
+@auth.route('/change-email/<token>')
+@login_required
+def change_email(token):
+    if current_user.change_email(token):
+        flash('Your email address has been updated.')
+    else:
+        flash('Invalid request.')
     return redirect(url_for('main.index'))
