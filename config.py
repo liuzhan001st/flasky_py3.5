1b43db005728b6236790789e000de72f5660d523
liuzhan001st <603812167@qq.com>
2016-08-19 00:10:12 +0800

    Chapter 11: Paginating Blog Post Lists (11d)

--- config.py
+++ config.py
@@ -10,6 +10,7 @@ class Config:
     FLASKY_MAIL_SENDER = 'liuzhan002nd <liuzhan002nd@163.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = True  # To supress a warning massege
+    FLASKY_POSTS_PER_PAGE = 20
 
     @staticmethod
     def init_app(app):
