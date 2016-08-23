98f2acf66aa240c23df7322b3325d832470327a2
liuzhan001st <603812167@qq.com>
2016-08-23 22:57:54 +0800

    Chapter 12: Self-Following (12e)

--- app/models.py
+++ app/models.py
@@ -104,6 +104,14 @@ class User(UserMixin, db.Model):
                 db.session.commit()
             except IntegrityError:
                 db.session.rollback()
+                
+    @staticmethod
+    def add_self_follows():
+        for user in User.query.all():
+            if not user.is_following(user):
+                user.follow(user)
+                db.session.add(user)
+                db.session.commit()
     
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
@@ -116,6 +124,8 @@ class User(UserMixin, db.Model):
         if self.email is not None and self.avatar_hash is None:
             self.avatar_hash = hashlib.md5(
                 self.email.encode('utf-8')).hexdigest()
+                
+        self.follow(self)
     
     def can(self, permissions):
         return self.role is not None and \
