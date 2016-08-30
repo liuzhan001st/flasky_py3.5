10226054f9680495a9dc08aef4d0606e467c452b
liuzhan001st <603812167@qq.com>
2016-08-30 22:59:30 +0800

    Chapter 13: Comments Submission and Display (13a)

--- config.py
+++ config.py
@@ -13,6 +13,7 @@ class Config:
     FLASKY_POSTS_PER_PAGE = 20
     FLASKY_FOLLOWERS_PER_PAGE = 20
     FLASKY_FOLLOWED_PER_PAGE = 20
+    FLASKY_COMMENTS_PER_PAGE = 30
     
     @staticmethod
     def init_app(app):
