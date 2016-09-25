b18cea4bc9f0008926519e1c5e2322f4c8ec4d1a
liuzhan001st <603812167@qq.com>
2016-09-25 10:59:19 +0800

    Chapter 16: Logging Slow Database Performance (16a)

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_RECORD_QUERIES = True
+    FLASKY_SLOW_DB_QUERY_TIME = 0.5
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'liuzhan002nd <liuzhan002nd@163.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
